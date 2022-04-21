e1b88342869a7544cbbb7d27183b72c13dc98e94
offmax.me@gmail.com <offmax.me@gmail.com>
2022-04-21 20:17:39 +0200

    1 commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+const SEP = '_';
 var field = $('#field')
-var mapSize = 5;
+var mapSize = 3;
 var blockSize = 50;
 
 startGame();
@@ -11,60 +12,35 @@ function startGame() {
     createMap();
 }
 function create(type, left, bottom) {
-    if (type === 'cornerLT') {
-        var html = `<div id="${left}${bottom}" background: url('textures/cornerLT0') onclick='', class="mapBlock" style= left: ${left}px; bottom: ${bottom}px">`;
-    }
-    if (type === 'cornerRT') {
-        var html = `<div id="${left}${bottom}" background: url('center0') onclick='', class="mapBlock" style= left: ${left}px; bottom: ${bottom}px"; transform: 'rotate(90grad)'>`;
-    }
-    console.log('LT')
+    var html = `<div id="${left}${SEP}${bottom}" background: url('textures/${type}') onclick='', class="mapBlock" style= left: ${left}px; bottom: ${bottom}px">`;
+    console.log(type);
     field.append(html);
 }
 
 function createMap() {
-    console.log('hiiii');
-      for (i = 0; i < mapSize; i++) {
-          for (j = 0; j < mapSize; j++) {
-              
-              
-              x = j * blockSize - blockSize / 3;
-              y = i * blockSize - blockSize / 3;
-              
-              
-              if(i == 0) {
-                  if(j == 0) {
-                    create('cornerLT', x, y);
-                  } else {
-                      if(j == mapSize) {
-                          create('cornerRT', x, y);
-                      } else {
-                          create('top', x, y);
-                      }
-                  }
-              } else {
-                  if(i == mapSize - 1) {
-                      if(j == 0) {
-                          create('cornerLB', x, y);
-                      } else {
-                          if(j == mapSize) {
-                              create('cornerRB', x, y);
-                          } else {
-                              create('bottom', x, y);
-                          }
-                      }
-                  } else {
-                      if(j == 1) {
-                          create('left', x, y);
-                      } else {
-                          if(j == mapSize) {
-                              create('right', x, y);
-                          } else {
-                              create('center', x, y)
-                          }
-                      }
-                  }
-              }
-          }
-      }
-      $('.mapBlock').show();
-  }
+    function createWhichBlock(left, center, right) {
+        if(i == 0) {
+            create(left, x, y);
+        } else if(i == mapSize - 1) {
+            create(right, x, y);
+        } else {
+            create(center, x, y);
+        }
+    }
+    for (j = 0; j < mapSize; j++) {
+        for (i = 0; i < mapSize; i++) {
+            
+            var x = Math.floor(i*(blockSize + blockSize / 3) + blockSize / 3);
+            var y = Math.floor(j*(blockSize + blockSize / 3) + blockSize / 3);
+            
+            if(j == 0) {
+                createWhichBlock('LB0', 'B0', 'RB0');
+            } else if(j == mapSize - 1) {
+                createWhichBlock('LT0', 'T0', 'RT0');
+            } else {
+                createWhichBlock('L0', 'C0', 'R0');
+            }
+        }
+    }
+  $('.mapBlock').show();
+}
