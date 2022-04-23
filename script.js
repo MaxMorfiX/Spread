0fa77651947554e0be88214a19113ede9e71176f
offmax.me@gmail.com <offmax.me@gmail.com>
2022-04-23 13:27:45 +0200

    Now map is gooooooood creating, added transform style

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const SEP = '_';
 var field = $('#field')
-var mapSize = 3;
+var mapSize = 4;
 var blockSize = 100;
 
 startGame();
@@ -14,23 +14,23 @@ function startGame() {
 function create(type, left, bottom) {
     
     if(type == 'LB') {
-        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Co.png'); left: ${left}px; bottom: ${bottom}px"></div>`;
+        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Co.png'); background-size: 100% 100%; transform: rotate(1500grad); left: ${left}px; bottom: ${bottom}px"></div>`;
     } else if(type == 'B') {
-        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Bo.png'); left: ${left}px; bottom: ${bottom}px"></div>`;
+        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Bo.png'); background-size: 100% 100%; transform: rotate(1500grad); left: ${left}px; bottom: ${bottom}px"></div>`;
     } else if(type  == 'RB') {
-        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Co.png'); left: ${left}px; bottom: ${bottom}px"></div>`;
+        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Co.png'); background-size: 100% 100%; transform: rotate(1000grad); left: ${left}px; bottom: ${bottom}px"></div>`;
     } else if(type  == 'L') {
-        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Bo.png'); left: ${left}px; bottom: ${bottom}px"></div>`;
+        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Bo.png'); background-size: 100% 100%; left: ${left}px; bottom: ${bottom}px"></div>`;
     } else if(type  == 'C') {
-        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Ce.png'); left: ${left}px; bottom: ${bottom}px"></div>`;
+        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Ce.png'); background-size: 100% 100%; left: ${left}px; bottom: ${bottom}px"></div>`;
     } else if(type  == 'R') {
-        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Bo.png'); left: ${left}px; bottom: ${bottom}px"></div>`;
+        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Bo.png'); background-size: 100% 100%; transform: rotate(1000grad); left: ${left}px; bottom: ${bottom}px"></div>`;
     } else if(type  == 'LT') {
-        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Co.png'); left: ${left}px; bottom: ${bottom}px"></div>`;
+        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Co.png'); background-size: 100% 100%; left: ${left}px; bottom: ${bottom}px"></div>`;
     } else if(type  == 'T') {
-        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Bo.png'); left: ${left}px; bottom: ${bottom}px"></div>`;
+        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Bo.png'); background-size: 100% 100%; transform: rotate(500grad); left: ${left}px; bottom: ${bottom}px"></div>`;
     } else if(type == 'RT') {
-        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Co.png'); left: ${left}px; bottom: ${bottom}px"></div>`;
+        var html = `<div id="${left}${SEP}${bottom}" onclick="" class="mapBlock" style=" background: url('textures/Co.png'); background-size: 100% 100%; left: ${left}px; transform: rotate(500grad); bottom: ${bottom}px"></div>`;
     }
     console.log(type);
     field.append(html);
@@ -50,12 +50,12 @@ function createMap() {
         for (i = 0; i < mapSize; i++) {
             
             var x = Math.floor(i*(blockSize + blockSize / 3));
-            var y = Math.floor(j*(blockSize + blockSize / 3));
+            var y = field.height() - blockSize - Math.floor(j*(blockSize + blockSize / 3));
             
             if(j == 0) {
-                createWhichBlock('LB', 'B', 'RB');
-            } else if(j == mapSize - 1) {
                 createWhichBlock('LT', 'T', 'RT');
+            } else if(j == mapSize - 1) {
+                createWhichBlock('LB', 'B', 'RB');
             } else {
                 createWhichBlock('L', 'C', 'R');
             }
