fa424db6edf694a20e5f9231049f811dc4366d38
offmax.me@gmail.com <offmax.me@gmail.com>
2022-05-15 16:26:28 +0200

    on full flying blocks appear at the stable blocks positions

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var mapSize = 3;
 var blockSize = 90;
 var energy = {};
 var batteries = {};
-var blocks = {};
 var flBl = {};
+var flBlCount = 0;
 var x;
 var y;
 var smallBlockNum;
@@ -13,7 +13,7 @@ var smallBlockNum;
 start();
 
 function start() {
-    mapSize = prompt("Name");
+//    mapSize = prompt("Name");
     startGame();
 }
 
@@ -33,8 +33,15 @@ function checkExplosions() {
     for(prop in energy) {
         if(energy[prop] == batteries[prop]['total']) {
             energy[prop] = 0;
-            $(`[id^='stBl']`).hide();
-//            for(i == 0, i < $(`[idˆ=stBl${SEP}${prop}]`).lenght())
+            for(i = 1; i <= 4; i++) {
+                if(batteries[prop][i]) {
+                    flBlCount++;
+                    flBl[flBlCount] = i;
+                    console.log($(`#stBl${SEP + prop + SEP + i}`).offset().left)
+                    create('flBl', $(`#stBl${SEP + prop + SEP + i}`).offset().left, field.width() - $(`#stBl${SEP + prop + SEP + i}`).offset().top + smallBlockSize, flBlCount);
+                }
+            }
+            $(`[id^='stBl${SEP}${prop}']`).hide();
         }
     }
 }
@@ -46,7 +53,7 @@ function addElement(x, y) {
         if(batteries[x + SEP + y][i]) {
             if(j == energy[x + SEP + y]) {
                 $('#stBl' + SEP + x + SEP + y + SEP + i).show();
-                console.log('add element  ' + i + '   ' + $('#stBl' + SEP + x + SEP + y + SEP + i).lenght);
+                console.log('add element  ' + i + '     ' + $('#stBl' + SEP + x + SEP + y + SEP + i).lenght);
                 break;
             } else {
                 j++;
@@ -151,12 +158,12 @@ function create(type, left, bottom, other) {
         var html = `<div id="${left}${SEP}${bottom}" onclick="addElement(${left}, ${bottom});" class="mapBlock" style=" background: url('textures/Bo.png'); background-size: 100% 100%; transform: rotate(500grad); left: ${left}px; bottom: ${bottom}px"></div>`;
     } else if(type == 'RT') {
         var html = `<div id="${left}${SEP}${bottom}" onclick="addElement(${left}, ${bottom});" class="mapBlock" style=" background: url('textures/Co.png'); background-size: 100% 100%; left: ${left}px; transform: rotate(500grad); bottom: ${bottom}px"></div>`;
-    } else if(type == 'flBlock') {
-        var html = `<div id="bl${blocks.lenght + 1}" class="block" style="background-color: red; color: red; left: ${left}px; bottom: ${bottom}px"></div>`;
     } else if(type == 'cycle') {
         var html = `<div id="cy${left}${SEP}${bottom}" class="cycle" style="background: url('textures/cycle.png'); background-size: 100% 100%; color: red; left: ${left}px; bottom: ${bottom}px"></div>`;
     } else if(type == 'block') {
         var html = `<div id="stBl${SEP + x + SEP + y + SEP + other}" class="block" style="background-color: red; color: red; left: ${left}px; bottom: ${bottom}px"></div>`;
+    } else if(type == 'flBl') {
+        var html = `<div id="flBl${SEP}${other}" class="block" style="display: block; background-color: red; color: red; left: ${left}px; bottom: ${bottom}px"></div>`;
     }
 //    console.log(type);
     field.append(html);
@@ -167,7 +174,7 @@ function create(type, left, bottom, other) {
 /*
 $(this).attr('id', 'something')
 
-    if(blocksIds[x + SEP + y] == 'LT') {
+    if(Ids[x + SEP + y] == 'LT') {
         if(energy[x + SEP + y] == 0) {
             create('cycle', x + 31.5, y + 31.5);
             create('block', x + 62, y + 32);
@@ -175,7 +182,7 @@ $(this).attr('id', 'something')
             create('block', x + 32, y + 2);
         }
          
-    } else if(blocksIds[x + SEP + y] == 'T') {
+    } else if(flBlIds[x + SEP + y] == 'T') {
         if(energy[x + SEP + y] == 0) {
             create('cycle', x + 31.5, y + 31.5);
             create('block', x + 2, y + 32);
@@ -184,7 +191,7 @@ $(this).attr('id', 'something')
         } else if (energy[x + SEP + y] == 2) {
              create('block', x + 32, y + 2);
          }
-    } else if(blocksIds[x + SEP + y] == 'C') {
+    } else if(flBlIds[x + SEP + y] == 'C') {
         if(energy[x + SEP + y] == 0) {
             create('cycle', x + 31.5, y + 31.5);
             create('block', x + 32, y + 62);
@@ -195,7 +202,7 @@ $(this).attr('id', 'something')
          } else if(energy[x + SEP + y] == 3) {
              create('block', x + 32, y + 2);
          }
-    }  else if (blocksIds[x + SEP + y] == 'L') {
+    }  else if (flBlIds[x + SEP + y] == 'L') {
          if (energy[x + SEP + y] == 0) {
              create('cycle', x + 31.5, y + 31.5);
              create('block', x + 32, y + 62);
@@ -204,7 +211,7 @@ $(this).attr('id', 'something')
          } else if (energy[x + SEP + y] == 2) {
              create('block', x + 32, y + 2);
          }
-    } else if (blocksIds[x + SEP + y] == 'R') {
+    } else if (flBlIds[x + SEP + y] == 'R') {
           if (energy[x + SEP + y] == 0) {
               create('cycle', x + 31.5, y + 31.5);
               create('block', x + 32, y + 62);
@@ -213,21 +220,21 @@ $(this).attr('id', 'something')
           } else if (energy[x + SEP + y] == 2) {
               create('block', x + 32, y + 2);
           }
-    } else if (blocksIds[x + SEP + y] == 'RT') {
+    } else if (flBlIds[x + SEP + y] == 'RT') {
            if (energy[x + SEP + y] == 0) {
                create('cycle', x + 31.5, y + 31.5);
                create('block', x + 2, y + 32);
            } else if (energy[x + SEP + y] == 1) {
                create('block', x + 32, y + 2);
            }
-    } else if (blocksIds[x + SEP + y] == 'LB') {
+    } else if (flBlIds[x + SEP + y] == 'LB') {
             if (energy[x + SEP + y] == 0) {
                 create('cycle', x + 31.5, y + 31.5);
                 create('block', x + 32, y + 62);
             } else if (energy[x + SEP + y] == 1) {
                 create('block', x + 62, y + 32);
             }
-    } else if (blocksIds[x + SEP + y] == 'B') {
+    } else if (flBlIds[x + SEP + y] == 'B') {
              if (energy[x + SEP + y] == 0) {
                  create('cycle', x + 31.5, y + 31.5);
                  create('block', x + 32, y + 62);
@@ -236,7 +243,7 @@ $(this).attr('id', 'something')
              } else if (energy[x + SEP + y] == 2) {
                  create('block', x + 62, y + 32);
              }
-    } else if (blocksIds[x + SEP + y] == 'RB') {
+    } else if (flBlIds[x + SEP + y] == 'RB') {
               if (energy[x + SEP + y] == 0) {
                   create('cycle', x + 31.5, y + 31.5);
                   create('block', x + 32, y + 62);
